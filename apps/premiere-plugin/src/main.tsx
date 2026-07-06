83bab70d8fc1e9d7d59a4b1ac34a123d9c863732
Anmol Saini <anmolsaini2k5@gmail.com>
2026-07-06 09:17:10 +0530

    feat: add Premiere UXP host adapter

--- apps/premiere-plugin/src/main.tsx
+++ apps/premiere-plugin/src/main.tsx
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { createMockPanelHost } from './host/mock-panel-host';
+import { createPremierePanelHost } from './host/premiere-uxp-adapter';
 import './styles.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App host={createMockPanelHost({ tracked: true })} />
-  </React.StrictMode>,
-);
+async function boot() {
+  const productionHost = await createPremierePanelHost();
+  const host = productionHost.project
+    ? productionHost
+    : createMockPanelHost({ tracked: true });
+
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App host={host} />
+    </React.StrictMode>,
+  );
+}
+
+void boot();
